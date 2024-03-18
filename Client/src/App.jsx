7bf19dbb76e780e288ef8046aed8c3f11edc7076
riteshjk <96072567+riteshjk@users.noreply.github.com>
2024-03-18 14:52:14 +0530

    done with navbar

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,20 +3,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './Pages/About';
 import SignIn from './Pages/SignIn';
 import SignUp from './Pages/SignUp';
-import Project from './Pages/Project';
+import Projects from './Pages/Projects';
 import DashBoard from './Pages/DashBoard';
 import Home from './Pages/Home';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <div>
       <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/project" element={<Project />} />
+        <Route path="/projects" element={<Projects />} />
         <Route path="/dashboard" element={<DashBoard />} />
       </Routes>
       </BrowserRouter>
