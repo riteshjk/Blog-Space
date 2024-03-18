c61afe6df063492d40d28a6dcf34d3aa0d1914b6
riteshjk <96072567+riteshjk@users.noreply.github.com>
2024-03-18 13:25:42 +0530

    donw with routing

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './Pages/About';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import Project from './Pages/Project';
+import DashBoard from './Pages/DashBoard';
+import Home from './Pages/Home';
 
 export default function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Hi</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
